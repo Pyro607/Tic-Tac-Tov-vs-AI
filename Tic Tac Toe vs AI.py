68a08594f021e985aea564463918c55562d38e90
Pyro607 <87315046+Pyro697@users.noreply.github.com>
2021-09-13 13:59:01 +0100

    Added printBoard function

--- Tic Tac Toe vs AI.py
+++ Tic Tac Toe vs AI.py
@@ -9,3 +9,16 @@ cpu_diff = 0
 board = [" " for i in range(10)]
 #range 10 instead of 9 to account for 0 index when player is making a move
 
+def printBoard(board):
+    print("   |   |")
+    print(" " + board[1] + " | " + board[2] + " | " + board[3])
+    print("   |   |")
+    print("-----------")
+    print("   |   |")
+    print(" " + board[4] + " | " + board[5] + " | " + board[6])
+    print("   |   |")
+    print("-----------")
+    print("   |   |")
+    print(" " + board[7] + " | " + board[8] + " | " + board[9])
+    print("   |   |")
+
