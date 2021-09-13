3bc2e425d52d5cffc769832156c893270f51afe9
Pyro607 <87315046+Pyro697@users.noreply.github.com>
2021-09-13 13:51:42 +0100

    Added cpu difficuilty and board variables. Added player or cpu setting if needed

--- Tic Tac Toe vs AI.py
+++ Tic Tac Toe vs AI.py
@@ -1,2 +1,11 @@
 #Tic Tac Toe vs AI
 
+#plyr_or_cpu = 0 needed?
+
+
+cpu_diff = 0
+#0 = easy, 1 = medium, 2 = hard, 3 = makes best move possible every round
+
+board = [" " for i in range(10)]
+#range 10 instead of 9 to account for 0 index when player is making a move
+
